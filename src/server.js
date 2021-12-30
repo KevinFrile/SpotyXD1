99f078bbba97e0d7d330fbc47897135cef09e094
Kevin Frile <kafraile@misena.edu.co>
2021-12-29 19:22:57 -0500

    v1

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const path = require('path');
+const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/dist/spoty-xd'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/spoty-xd/index.html'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/spoty-xd'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/spoty-xd/index.html'));
 });
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
